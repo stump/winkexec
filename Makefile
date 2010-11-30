2831e348fe0c3261ef6baff792008663b4922a95
John Stumpo <stump@jstump.com>
2010-11-30 02:52:06 -0500

    Find the kernel directly in memory rather than looking in its image on disk.
    (This (ab)uses the undocumented ZwQuerySystemInformation function. But
    since we're using it just to hook the kernel's import of another
    undocumented function from the HAL...)
    
    libpe no longer exists, and there is no longer a dependency on a
    kernel image that matches the running one being present in system32/
    at reboot time.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # WinKexec: kexec for Windows
-# Copyright (C) 2008-2009 John Stumpo
+# Copyright (C) 2008-2010 John Stumpo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- driver/reboot.c
+++ driver/reboot.c
@@ -1,5 +1,5 @@
 /* WinKexec: kexec for Windows
- * Copyright (C) 2008-2009 John Stumpo
+ * Copyright (C) 2008-2010 John Stumpo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,6 @@
 
 /* Hook system reboot, and define the stuff needed to handle it. */
 
-#include "libpe.h"
 #include "linuxboot.h"
 #include "reboot.h"
 #include "buffer.h"
@@ -36,6 +35,79 @@ typedef VOID KEXEC_NORETURN NTAPI(*halReturnToFirmware_t)(FIRMWARE_REENTRY);
 
 static halReturnToFirmware_t real_HalReturnToFirmware;
 
+/* More undocumentedness... */
+typedef enum {
+  SystemModuleInformation = 11
+} SYSTEM_INFORMATION_CLASS;
+
+typedef struct {
+  ULONG Reserved1;
+  ULONG Reserved2;
+#ifdef __x86_64__
+  ULONG Reserved3;
+  ULONG Reserved4;
+#endif
+  PVOID ImageBaseAddress;
+  ULONG ImageSize;
+  ULONG Flags;
+  WORD Id;
+  WORD Rank;
+  WORD Unknown1;
+  WORD NameOffset;
+  BYTE Name[256];
+} SYSTEM_MODULE;
+
+typedef struct {
+  ULONG ModulesCount;
+  SYSTEM_MODULE Modules[0];
+} SYSTEM_MODULE_INFORMATION;
+
+NTSTATUS NTAPI ZwQuerySystemInformation(SYSTEM_INFORMATION_CLASS, PVOID, ULONG, PULONG);
+
+
+static PVOID find_kernel_base(void)
+{
+  SYSTEM_MODULE_INFORMATION* module_info = NULL;
+  ULONG length = 0;
+  int i;
+  int j;
+  const PCHAR kernel_filenames[] = {"ntoskrnl.exe", "ntkrnlpa.exe",
+                                    "ntkrnlmp.exe", "ntkrpamp.exe"};
+  const int num_kernel_filenames = 4;
+
+  /* Call QSI once to find out how much room to allocate for its output.
+     TODO: look up correct error code for having to alloc the buffer */
+  ZwQuerySystemInformation(SystemModuleInformation,
+    module_info, length, &length);
+  module_info = ExAllocatePoolWithTag(NonPagedPool,
+    length, TAG('K', 'x', 'e', 'c'));
+
+  /* Once more, with feeling. */
+  if (!NT_SUCCESS(ZwQuerySystemInformation(SystemModuleInformation,
+    module_info, length, &length)))
+  {
+    ExFreePool(module_info);
+    return NULL;
+  }
+
+  /* Find the kernel in the module list. */
+  for (i = 0; i < module_info->ModulesCount; i++) {
+    for (j = 0; j < num_kernel_filenames; j++) {
+      if (strcasecmp(module_info->Modules[i].Name,
+          kernel_filenames[j]) == 0)
+      {
+        PVOID kernel_base = module_info->Modules[i].ImageBaseAddress;
+        ExFreePool(module_info);
+        return kernel_base;
+      }
+    }
+  }
+
+  ExFreePool(module_info);
+  return NULL;
+}
+
+
 /* Our "enhanced" version of HalReturnToFirmware.
    Drops through if we don't have a kernel to load or if an invalid
    operation type is specified.  The guts of ntoskrnl.exe will be
@@ -53,52 +125,49 @@ static VOID KEXEC_NORETURN NTAPI KexecDoReboot(FIRMWARE_REENTRY RebootType)
 
 NTSTATUS KexecHookReboot(void)
 {
-  PVOID Ntoskrnl = NULL;
   PVOID KernelBase;
-  DWORD ImportOffset;
-  halReturnToFirmware_t* Target;
+  PIMAGE_NT_HEADERS NtHeaders;
+  PIMAGE_IMPORT_DESCRIPTOR ImportDescriptor;
+  halReturnToFirmware_t* Target = NULL;
   PMDL Mdl;
-  int i;
-  PWCHAR KernelFilenames[] = {L"ntoskrnl.exe", L"ntkrnlpa.exe",
-                              L"ntkrnlmp.exe", L"ntkrpamp.exe", NULL};
 
-  for (i = 0; KernelFilenames[i]; i++) {
-    if (!(Ntoskrnl = PeReadSystemFile(KernelFilenames[i])))
-      return STATUS_INSUFFICIENT_RESOURCES;
-
-    /* Compute base load address of ntoskrnl.exe by doing this to
-       an arbitrary function from ntoskrnl.exe. */
-    KernelBase = KeBugCheckEx - PeGetExportFunction(Ntoskrnl, "KeBugCheckEx");
-    if (KernelBase == KeBugCheckEx) {
-      ExFreePool(Ntoskrnl);
-      Ntoskrnl = NULL;
-      continue;
-    }
-
-    /* Make sure it's right. */
-    if (!MmIsAddressValid(KernelBase) || !PeGetNtHeaders(KernelBase)) {
-      ExFreePool(Ntoskrnl);
-      Ntoskrnl = NULL;
-      continue;
-    }
-
-    break;
-  }
-
-  if (!Ntoskrnl)
-    return STATUS_UNSUCCESSFUL;
-
-  if (!(ImportOffset = PeGetImportPointer(Ntoskrnl, "hal.dll", "HalReturnToFirmware"))) {
-    ExFreePool(Ntoskrnl);
+  /* Find the kernel. */
+  KernelBase = find_kernel_base();
+  if (KernelBase == NULL
+       || !MmIsAddressValid(KernelBase)
+       || ((PIMAGE_DOS_HEADER)KernelBase)->e_magic != 0x5a4d)
+  {
+    DbgPrint("Unable to find kernel base address.\n");
     return STATUS_UNSUCCESSFUL;
   }
 
-  ExFreePool(Ntoskrnl);
+  /* Find the kernel's import table. */
+  NtHeaders = KernelBase + ((PIMAGE_DOS_HEADER)KernelBase)->e_lfanew;
+  ImportDescriptor = KernelBase +
+    NtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
 
-  Target = (halReturnToFirmware_t*)(KernelBase + ImportOffset);
+  /* Find the kernel's import of HalReturnToFirmware from hal.dll. */
+  while (ImportDescriptor->Name != 0) {
+    if (strcasecmp(KernelBase + ImportDescriptor->Name, "hal.dll") == 0) {
+      PIMAGE_THUNK_DATA NameThunk, CallThunk;
 
-  /* Here we go!
-     We need to unprotect the chunk of RAM the import table is in. */
+      for (NameThunk = KernelBase + ImportDescriptor->OriginalFirstThunk,
+           CallThunk = KernelBase + ImportDescriptor->FirstThunk;
+        NameThunk->u1.AddressOfData != 0; NameThunk++, CallThunk++)
+      {
+        PIMAGE_IMPORT_BY_NAME NamedImport = KernelBase + NameThunk->u1.AddressOfData;
+        if (strcmp(NamedImport->Name, "HalReturnToFirmware") == 0)
+          Target = (halReturnToFirmware_t*)CallThunk;
+      }
+    }
+  }
+
+  if (Target == NULL) {
+    DbgPrint("Unable to find kernel import descriptor for hal.dll.");
+    return STATUS_UNSUCCESSFUL;
+  }
+
+  /* Make it read-write. */
   if (!(Mdl = IoAllocateMdl(Target, sizeof(halReturnToFirmware_t), FALSE, FALSE, NULL)))
     return STATUS_UNSUCCESSFUL;
   MmBuildMdlForNonPagedPool(Mdl);
@@ -109,8 +178,10 @@ NTSTATUS KexecHookReboot(void)
     IoFreeMdl(Mdl);
     return STATUS_UNSUCCESSFUL;
   }
+  /* Hook it. */
   real_HalReturnToFirmware = *Target;
-  *Target = KexecDoReboot;  /* This is it! */
+  *Target = KexecDoReboot;
+  /* And clean up. */
   MmUnmapLockedPages(Target, Mdl);
   IoFreeMdl(Mdl);
 
